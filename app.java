a01807ebb6887e098aa398de16ccfe2ee8882907
root <root@ip-172-31-30-5.ec2.internal>
2025-11-09 05:03:02 +0000

    java code

---
+++ app.java
@@ -0,0 +1,7 @@
+# Java Code starts here//
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Prints "Hello, World!" to the console
+    }
+}
+
